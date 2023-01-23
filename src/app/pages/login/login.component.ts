42162c6e929c0229d85c3c90717955a56fea14ab
Riad MAHI <riad773@hotmail.fr>
2023-01-23 11:31:37 +0100

    MR check done

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -12,15 +12,6 @@ export class LoginComponent implements OnInit {
     
   }
   ngOnInit(): void {
-    const signInButton = document.getElementById('signIn');
-    const signUpButton = document.getElementById('signUp');
-    const container = document.getElementById('container');
-    signInButton?.addEventListener('click', () => {
-      container?.classList.remove("right-panel-active");
-    });
-    signUpButton?.addEventListener('click', () => {
-      container?.classList.add("right-panel-active");
-    });
   }
 
 
