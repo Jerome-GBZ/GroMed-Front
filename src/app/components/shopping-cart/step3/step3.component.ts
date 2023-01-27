b2caec54135b2deb90f625f001db50bd642cbce0
jerome-gbz <jerome.gambiez@etu.univ-grenoble-alpes.fr>
2023-01-27 11:18:15 +0100

    add design step3

--- src/app/components/shopping-cart/step3/step3.component.ts
+++ src/app/components/shopping-cart/step3/step3.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { PresentationPanierModel } from 'src/libs';
+import { PresentationPanierModel, LivraisonModel } from 'src/libs';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-step3',
@@ -9,8 +10,13 @@ import { PresentationPanierModel } from 'src/libs';
 export class Step3Component {
   @Input() total: number = 0;
   @Input() medicamentsLine: Array<PresentationPanierModel> = new Array();
+  @Input() livraison: LivraisonModel | undefined;
 
   @Output() stepItemEvent = new EventEmitter<number>();
+
+  options: AnimationOptions = {
+    path: '/assets/lottie/delivery.json'
+  };
 
   nextStep(value: number) {
     this.stepItemEvent.emit(value);
